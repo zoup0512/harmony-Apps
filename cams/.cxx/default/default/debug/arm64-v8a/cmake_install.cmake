512fe9a0e0952a9543e228f7fb353132af885383
zoup0512 <2479008771@qq.com>
2026-07-20 22:55:38 +0800

    chore(cmake): 清理构建缓存和配置文件
    - 删除了 CMake 缓存文件 cache-v2-6e6092a8c2a531015f8b.json
    - 移除了 cmakeFiles-v1-fb3a97a2853670a4f3ad.json 配置信息
    - 清理了 arm64-v8a 架构的调试构建配置
    - 更新了项目构建环境设置

--- cams/.cxx/default/default/debug/arm64-v8a/cmake_install.cmake
+++ cams/.cxx/default/default/debug/arm64-v8a/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/24790/DevEcoStudioProjects/github/harmony-Apps/cams/src/main/cpp
+# Install script for directory: D:/workspace/My/harmony/harmony-Apps/cams/src/main/cpp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/24790/DevEcoStudioProjects/github/harmony-Apps/cams/.cxx/default/default/debug/arm64-v8a/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/workspace/My/harmony/harmony-Apps/cams/.cxx/default/default/debug/arm64-v8a/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
